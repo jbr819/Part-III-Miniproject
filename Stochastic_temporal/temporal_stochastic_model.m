e177f2caee7b309e70f223eb6d6918e90b23f957
Finley Webb <fw347@cam.ac.uk>
2023-03-07 15:00:24 +0000

    Updated stochastic temporal model, attempting to add keratinocyte population

--- Stochastic_temporal/temporal_stochastic_model.m
+++ Stochastic_temporal/temporal_stochastic_model.m
@@ -1,27 +1,29 @@
 % Initialise run variables
-X=100;  % initial conditions
-k=60; % birth rate coefficient
-lambda=0.1; % death rate coefficient
+prolifRate=0.0275; % birth rate coefficient
+diffRate=0.0275; % death rate coefficient
+asymRate=0.195; % rate of asymmetric division
+X = [10; 0]; % for this model equilibrium population is equal to prolif rate / diff rate
 
-N=1000; % Number of jumps
+N=100000; % Number of jumps
 t=zeros(N,1);  % array containing time
 dt=zeros(N,1); % array containing time intervals
-z=zeros(N,1);  % state variable array (contains population)
+z=zeros(N,2);  % state variable array (contains population)
 z(1,:)=[X];    % initial values input into first array of state variabl array
 
 for i=2:N
-    x=z(i-1,1); % x in the previous state variable
-    rates=[k lambda*x]; %the parameters used to work out rates
-    R=[1 -1]; % no idea what this is
-    lam=sum(rates); % 
-    dt(i-1)= -log(rand)/lam;
-    t(i)=t(i-1)+dt(i-1);
-    rates=rates/lam;
-    reac=1+sum(rand>cumsum(rates));
-    z(i,:)=z(i-1,:)+R(:,reac)';
+    x=z(i-1,1); % set previous pop sizre as current size
+    rates=[prolifRate*x asymRate*x diffRate*x]; %set birth and deeath rates based on current pop
+    R=[1,-1; 0, 1; -1, 2]; % set change in pop size for each of the two rates
+    lam=sum(rates); % calculate the total rate. Parameter of expo. dist. used to determine next event is the totla rate of all possible events
+    dt(i-1)= -log(rand)/lam; %set time interval based on the exponenetial distribution
+    t(i)=t(i-1)+dt(i-1); %update time array
+    rates=rates/lam; %normalise the rates
+    reac=1+sum(rand>cumsum(rates)); %determine which event occured based on normalised rates
+    z(i,:)=z(i-1,:)+R(:,reac)'; %update pop size based on event that occured
 end
 
 plot(t,z);
+hold on
 
 % Generate weighted mean (last element not used)
 transpose(dt)*z/sum(dt)
@@ -31,5 +33,5 @@ var(z,dt)
 
 
 %% implement a birth death process with a second reaction where it goes back to itself
-%% plot a graph of how this p(asymmetric division) affects the variance
+%% plot a graph of how this p(asymmetric division) affects the variance and the extinction distribution
 %% set birthrate to 1 to fit death rate parameter
