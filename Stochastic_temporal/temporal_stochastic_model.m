198c886566cebc7c5627f885f23b83b0a7f096f8
Finley Webb <fw347@cam.ac.uk>
2023-03-07 16:03:07 +0000

    Updating stochastic temporal model

--- Stochastic_temporal/temporal_stochastic_model.m
+++ Stochastic_temporal/temporal_stochastic_model.m
@@ -2,7 +2,8 @@
 prolifRate=0.0275; % birth rate coefficient
 diffRate=0.0275; % death rate coefficient
 asymRate=0.195; % rate of asymmetric division
-X = [10; 0]; % for this model equilibrium population is equal to prolif rate / diff rate
+keratinLoss=0;
+X = [10, 0]; % for this model equilibrium population is equal to prolif rate / diff rate
 
 N=100000; % Number of jumps
 t=zeros(N,1);  % array containing time
@@ -11,8 +12,9 @@ z=zeros(N,2);  % state variable array (contains population)
 z(1,:)=[X];    % initial values input into first array of state variabl array
 
 for i=2:N
-    x=z(i-1,1); % set previous pop sizre as current size
-    rates=[prolifRate*x asymRate*x diffRate*x]; %set birth and deeath rates based on current pop
+    x=z(i-1,1); % set previous pop of stem cells as current size
+    y = z(i-1, 2); % set previous pop of keratinocyte cells as current size
+    rates=[prolifRate*x asymRate*x diffRate*x]; %set birth and death rates based on current pop
     R=[1,-1; 0, 1; -1, 2]; % set change in pop size for each of the two rates
     lam=sum(rates); % calculate the total rate. Parameter of expo. dist. used to determine next event is the totla rate of all possible events
     dt(i-1)= -log(rand)/lam; %set time interval based on the exponenetial distribution
